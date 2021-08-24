f15ce1536a13b1fc25b73430a8aea77692e9509a
Nickolay Bogdanov <nickolay.bogdanov@mheducation.com>
2021-08-24 08:35:34 +0300

    removed extra files

--- projects/mat-menu-triangle/src/lib/mat-menu-triangle.module.ts
+++ projects/mat-menu-triangle/src/lib/mat-menu-triangle.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { MatMenuTriangleDirective } from './mat-menu-triangle.directive';
 
 
-
 @NgModule({
   declarations: [
     MatMenuTriangleDirective,
--- projects/mat-menu-triangle/src/public-api.ts
+++ projects/mat-menu-triangle/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of mat-menu-triangle
  */
 
-export * from './lib/mat-menu-triangle.service';
-export * from './lib/mat-menu-triangle.component';
+export * from './lib/mat-menu-triangle.directive';
 export * from './lib/mat-menu-triangle.module';
